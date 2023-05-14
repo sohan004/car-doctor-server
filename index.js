3fd7b07bfa93a006816f2002b434e2b2d77260ca
sohan004 <aryansohan02@gmail.com>
2023-05-14 18:39:49 +0600

    update

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+        // await client.connect();
         const database = client.db("serviceDB").collection("service");
         const bookmark = client.db("bookmarkDB").collection("bookmark");
 
